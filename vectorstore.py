565b8a04d68b419f3c7252de0f984fabce1c2ff7
Atharva Kamble <amkamble371122@kkwagh.edu.in>
2026-03-10 00:47:18 +0530

    Fixed vectorstore for deployment

--- vectorstore.py
+++ vectorstore.py
@@ -1,19 +1,29 @@
 from langchain_community.vectorstores import Chroma
+import os
+
 
 def create_vector_store(chunks, embeddings):
+
+    os.makedirs("chroma_db", exist_ok=True)
+
     vectordb = Chroma.from_documents(
         documents=chunks,
         embedding=embeddings,
         persist_directory="chroma_db"
     )
+
     vectordb.persist()
+
     return vectordb
 
-from langchain_community.vectorstores import Chroma
 
 def load_vector_store(embeddings):
+
+    os.makedirs("chroma_db", exist_ok=True)
+
     vectordb = Chroma(
         persist_directory="chroma_db",
         embedding_function=embeddings
     )
-    return vectordb    
+
+    return vectordb
