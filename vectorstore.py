b945a70fd0c5153b4690ea50b03e73a5c336692f
Atharva Kamble <amkamble371122@kkwagh.edu.in>
2026-03-10 01:51:24 +0530

    Build vector database dynamically

--- vectorstore.py
+++ vectorstore.py
@@ -1,33 +1,20 @@
 from langchain_community.vectorstores import Chroma
-import os
-
-# Absolute path to project directory
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "chroma_db")
-
-
-def create_vector_store(chunks, embeddings):
-
-    os.makedirs(DB_PATH, exist_ok=True)
-
-    vectordb = Chroma.from_documents(
-        documents=chunks,
-        embedding=embeddings,
-        persist_directory=DB_PATH
-    )
-
-    vectordb.persist()
-
-    return vectordb
+from loader import load_documents
+from chunking import split_documents
 
 
 def load_vector_store(embeddings):
 
-    os.makedirs(DB_PATH, exist_ok=True)
+    # Load PDFs from data folder
+    documents = load_documents()
 
-    vectordb = Chroma(
-        persist_directory=DB_PATH,
-        embedding_function=embeddings
+    # Split into chunks
+    chunks = split_documents(documents)
+
+    # Create vector database in memory
+    vectordb = Chroma.from_documents(
+        documents=chunks,
+        embedding=embeddings
     )
 
     return vectordb
