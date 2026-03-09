3ee9c3ff73d022fdc359e4b67cfe6b4202210f81
Atharva Kamble <amkamble371122@kkwagh.edu.in>
2026-03-10 01:36:25 +0530

    Fix ChromaDB path for deployment

--- vectorstore.py
+++ vectorstore.py
@@ -1,15 +1,19 @@
 from langchain_community.vectorstores import Chroma
 import os
 
+# Absolute path to project directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "chroma_db")
+
 
 def create_vector_store(chunks, embeddings):
 
-    os.makedirs("chroma_db", exist_ok=True)
+    os.makedirs(DB_PATH, exist_ok=True)
 
     vectordb = Chroma.from_documents(
         documents=chunks,
         embedding=embeddings,
-        persist_directory="chroma_db"
+        persist_directory=DB_PATH
     )
 
     vectordb.persist()
@@ -19,10 +23,10 @@ def create_vector_store(chunks, embeddings):
 
 def load_vector_store(embeddings):
 
-    os.makedirs("chroma_db", exist_ok=True)
+    os.makedirs(DB_PATH, exist_ok=True)
 
     vectordb = Chroma(
-        persist_directory="chroma_db",
+        persist_directory=DB_PATH,
         embedding_function=embeddings
     )
 
